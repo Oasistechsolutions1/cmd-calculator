e436846e9992190aed30dbcf8e702d6dfaa79640
Oasistechsolutions1 <khanballpoolgamer@gmail.com>
2024-03-22 10:02:14 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    {message: "input first num", type: "number" , name: "firstnum",},
+    {message: "input second num", type: "number" , name: "secondnum",},
+    {message: "select one of the operator to perfome action", type: "list" , name: "operator",
+choices:["addition", "subtraction", "multiplication","division"],},
+]);
+
+if(answer.operator === "addition")
+{console.log('your answer is :' + answer.firstnum + answer.secondnum)}
+else if(answer.operator === "subtraction")
+{console.log(answer.firstnum - answer.secondnum)}
+else if(answer.operator === "multiplication")
+{console.log(answer.firstnum * answer.secondnum)}
+else if(answer.operator === "division")
+{console.log(answer.firstnum / answer.secondnum)}
